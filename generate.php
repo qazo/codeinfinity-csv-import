8b9fe66de7b9c7ba2903b152082712cb936600eb
Kwezilomso Mhaga <kwezimhaga@live.com>
2021-09-23 14:22:29 +0200

    add page for CSV generation

--- generate.php
+++ generate.php
@@ -1,3 +1,13 @@
+<?php
+require_once(__DIR__ . "/vendor/autoload.php");
+
+if (filter_has_var(INPUT_POST, "submit")) {
+	$recordCount = $_POST["recordCount"];
+	$outputFilePath = \App\RandomCsvGenerator::generateRandomisedCsv($recordCount);
+
+	$message = "CSV file with {$recordCount} random and unique records successfully created and stored in '{$outputFilePath}'.<br>";
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,5 +20,33 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
 </head>
 <body>
+	<nav class="navbar navbar-dark bg-primary">
+		<div class="container">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="index.php">CSV import</a>
+			</div>
+		</div>
+	</nav>
+
+	<div class="container">
+		<h3>Generate CSV file</h3>
+		<?php if (isset($message) && strlen($message) > 0): ?>
+		<div class="alert alert-success">
+			<?php echo $message; ?>
+		</div>
+		<?php endif ?>
+
+		<form method="post">
+			<div class="form-group">
+				<div class="input-group mb-3">
+					<input name="recordCount" class="form-control" type="number" min="1"
+						placeholder="Record count">
+					<button name="submit" class="btn btn-primary" type="submit">Submit</button>
+				</div>
+			</div>
+			<br>
+			<a class="btn btn-secondary" href="index.php">Cancel</a>
+		</form>
+	</div>
 </body>
 </html>
