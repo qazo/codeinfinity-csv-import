f4ceb1ef0ac86a0a83ffa820ec6429ce856c7cb6
Kwezilomso Mhaga <kwezimhaga@live.com>
2021-09-23 12:43:29 +0200

    generate unique dir for output.csv

--- src/RandomCsvGenerator.php
+++ src/RandomCsvGenerator.php
@@ -5,14 +5,23 @@ require_once(__DIR__ . "/../vendor/autoload.php");
 class RandomCsvGenerator
 {
 	const DATE_FORMAT = "d/m/Y";
+	const OUTPUT_FILE_DIR = __DIR__ . "/../csv-output";
+
+	private static function getUniqueOutputFilePath()
+	{
+		$currDateTime = new \DateTime("now");
+		$currTimeStamp = $currDateTime->getTimestamp();
+		return self::OUTPUT_FILE_DIR . "/file-out_{$currTimeStamp}/output.csv";
+	}
 
 	// returns file path
-	public static function generateRandomisedCsv(string $filepath, int $recordCount): string
+	public static function generateRandomisedCsv(int $recordCount): string
 	{
 		$nameGenerator = new \App\UniqueNameGenerator();
 		$dobGenerator = new \App\RandomDateOfBirthGenerator();
 		$currentDate = new \DateTime("today");
 
+		$filepath = self::getUniqueOutputFilePath();
 		$fileHandle = fopen($filepath, "w");
 		// add headers
 		fputcsv($fileHandle, ["Id", "Name", "Surname", "Initials", "Age", "DateOfBirth"]);
