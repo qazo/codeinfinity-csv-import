4a5a73bc09fa09b8e03a168ea00266cb1a313267
Kwezilomso Mhaga <kwezimhaga@live.com>
2021-09-24 07:00:21 +0200

    csv importing

--- import.php
+++ import.php
@@ -1,3 +1,27 @@
+<?php
+require_once(__DIR__ . "/vendor/autoload.php");
+
+function fileUploadOk()
+{
+	if ($_FILES["csvFile"]["error"] != UPLOAD_ERR_OK) {
+		return false;
+	}
+	if (!is_uploaded_file($_FILES["csvFile"]["tmp_name"])) {
+		return false;
+	}
+	return true;
+}
+
+if (filter_has_var(INPUT_POST, "submit") && fileUploadOk()) {
+	// should i rather read line by line to potentially save mem?
+	$csvFilePath = $_FILES["csvFile"]["tmp_name"];
+	$csvImporter = new \App\CsvUserImporter();
+	$recordCount = $csvImporter->importCsvToDb($csvFilePath);
+
+	$message = "CSV file with {$recordCount} random and unique records successfully created and stored in '{$outputFilePath}'.<br>";
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,5 +34,35 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
 </head>
 <body>
+	<nav class="navbar navbar-dark bg-primary">
+		<div class="container">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="index.php">CSV import</a>
+			</div>
+		</div>
+	</nav>
+
+	<div class="container">
+		<br>
+		<?php if (isset($message) && strlen($message) > 0): ?>
+		<div class="alert alert-success">
+			<?php echo $message; ?>
+		</div>
+		<?php endif ?>
+
+		<form enctype="multipart/form-data" method="post">
+			<legend>Import file</legend>
+			<div class="form-group">
+				<div class="input-group mb-3">
+					<label class="form-label mt-4" for="csvFile"></label>
+					<input name="csvFile" class="form-control" type="file">
+				</div>
+			</div>
+			<br>
+
+			<button type="submit" name="submit" class="btn btn-primary">Submit</button>
+			<a class="btn btn-secondary" href="index.php">Cancel</a>
+		</form>
+	</div>
 </body>
 </html>
---
+++ src/CsvUserImporter.php
@@ -0,0 +1,57 @@
+<?php
+namespace App;
+
+class CsvUserImporter
+{
+	const INSERT_USER_SQL = "insert into csv_import(name, surname, initials, age, date_of_birth) values (?, ?, ?, ?, ?);";
+	const CREATE_TABLE_SQL = "create table csv_import(id integer primary key autoincrement, name text, surname text, initials text, age int, date_of_birth text);";
+	const OUTPUT_FILE_DIR = __DIR__ . "/../csv-output";
+
+	private static function getUniqueOutputFilePath()
+	{
+		$currDateTime = new \DateTime("now");
+		$currTimeStamp = $currDateTime->getTimestamp();
+		$uniqueDirPath = self::OUTPUT_FILE_DIR . "/file-out_{$currTimeStamp}";
+		mkdir($uniqueDirPath, 0755, true);
+		return $uniqueDirPath . "/csv-import.sqlite3";
+	}
+
+	public function importCsvToDb(string $csvFilePath, string $dbFilePath = ""): int
+	{
+		if (!isset($dbFilePath) || $dbFilePath == "") {
+			$dbFilePath = self::getUniqueOutputFilePath();
+		}
+
+		$csvFileHandle = fopen($csvFilePath, "r");
+		$line = fgetcsv($csvFileHandle); // skip first line
+
+		$name = "";
+		$surname = "";
+		$initials = "";
+		$age = 0;
+		$dateOfBirth = "";
+
+		$db = new \SQLite3($dbFilePath);
+		$db->exec(self::CREATE_TABLE_SQL);
+
+		$statement = $db->prepare(self::INSERT_USER_SQL);
+		$statement->bindParam(1, $name);
+		$statement->bindParam(2, $surname);
+		$statement->bindParam(3, $initials);
+		$statement->bindParam(4, $age);
+		$statement->bindParam(5, $dateOfBirth);
+
+		while (($line = fgetcsv($csvFileHandle, 1000)) != false) {
+			$name = $line[1];
+			$surname = $line[2];
+			$initials = $line[3];
+			$age = $line[4];
+			$dateOfBirth = $line[5];
+			$statement->execute();
+			$recordCount++;
+		}
+		fclose($csvFilePath);
+		return $recordCount;
+	}
+}
+?>
